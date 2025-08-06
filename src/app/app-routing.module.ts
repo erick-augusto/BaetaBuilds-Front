5f6e5a77eee627f21433b74d545f2eda867af64c
erick-augusto <erick_aug@hotmail.com>
2025-08-06 20:22:26 -0300

    Criando rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './componentes/corpo/home/home.component';
+import { CadastrarTerritorioComponent } from './componentes/corpo/territorio/cadastrar-territorio/cadastrar-territorio.component';
+import { ListarTerritorioComponent } from './componentes/corpo/territorio/listar-territorio/listar-territorio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },  
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'listarTerritorio',
+    component: ListarTerritorioComponent
+  },  
+  {
+    path: 'cadastrarTerritorio',
+    component: CadastrarTerritorioComponent
+  }  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
 import { RodapeComponent } from './componentes/rodape/rodape.component';
+import { HomeComponent } from './componentes/corpo/home/home.component';
+import { CadastrarTerritorioComponent } from './componentes/corpo/territorio/cadastrar-territorio/cadastrar-territorio.component';
+import { ListarTerritorioComponent } from './componentes/corpo/territorio/listar-territorio/listar-territorio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CabecalhoComponent,
-    RodapeComponent
+    RodapeComponent,
+    HomeComponent,
+    CadastrarTerritorioComponent,
+    ListarTerritorioComponent
   ],
   imports: [
     BrowserModule,
