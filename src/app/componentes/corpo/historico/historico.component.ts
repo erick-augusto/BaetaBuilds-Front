b5102464e23a2e9b2e220fc0f45219fcf68c6d79
erick-augusto <erick_aug@hotmail.com>
2025-10-03 19:41:50 -0300

    Criando componente de histórico

---
+++ src/app/componentes/corpo/historico/historico.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { HistoricoDTO } from '../territorio/modelos/historicoDTO';
+import { HistoricoService } from '../territorio/services/historico.service';
+
+@Component({
+  selector: 'app-historico',
+  templateUrl: './historico.component.html',
+  styleUrls: ['./historico.component.css']
+})
+export class HistoricoComponent implements OnInit {
+
+  historico!: HistoricoDTO;
+
+  constructor(private service: HistoricoService) { }
+
+  ngOnInit(): void {
+    this.service.buscarHistorico().subscribe((resposta) => {
+      this.historico = resposta;
+    });
+  }
+
+}
