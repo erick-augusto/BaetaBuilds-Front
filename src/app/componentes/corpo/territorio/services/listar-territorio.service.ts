1ff710e25b21576afa48424f3c2f017275cf3ba2
erick-augusto <erick_aug@hotmail.com>
2025-11-25 19:47:58 -0300

    Incluindo envio do token no serviço de listagem

--- src/app/componentes/corpo/territorio/services/listar-territorio.service.ts
+++ src/app/componentes/corpo/territorio/services/listar-territorio.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { TerritorioDTO } from '../modelos/territorioDTO';
 import { Observable } from 'rxjs/internal/Observable';
@@ -13,6 +13,13 @@ export class ListarTerritorioService {
   constructor(private http: HttpClient) { }
 
   listarTerritorios(): Observable<TerritorioDTO[]>{
-    return this.http.get<TerritorioDTO[]>(this.API);
+    const token = localStorage.getItem('token');
+    console.log('Token recuperado do localStorage:', token);
+    const headers = token
+    ? new HttpHeaders({ 'Content-Type': 'application/json', Authorization: `Bearer ${token}` })
+    : new HttpHeaders({ 'Content-Type': 'application/json' });
+    console.log('Usando token nos headers:', headers.get('Authorization'));
+
+    return this.http.get<TerritorioDTO[]>(this.API, { headers });
   }
 }
