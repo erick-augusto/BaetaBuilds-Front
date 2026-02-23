c7cfe3dc0dda1f016203d083b5c5d100e2ed4907
erick-augusto <erick_aug@hotmail.com>
2026-02-23 15:37:12 -0300

    Exibindo lista de apartamentos

--- src/app/componentes/corpo/territorio/modelos/territorio-dto/territorio-dto.component.ts
+++ src/app/componentes/corpo/territorio/modelos/territorio-dto/territorio-dto.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { TerritorioDTO } from '../territorioDTO';
+import { EnviarSaidaDTO } from '../enviarSaidaDTO';
+import { ListarApartamentosService } from '../../services/listar-apartamentos.service';
 
 @Component({
   selector: 'app-territorio-dto',
@@ -15,9 +17,40 @@ export class TerritorioDTOComponent implements OnInit {
     totalAptos: 10
   }
 
-  constructor() { }
+  expanded: boolean = false;
+  apartamentos: EnviarSaidaDTO[] = [];
+  carregando: boolean = false;
+  erro: string = '';
+
+  constructor(private listarApartamentosService: ListarApartamentosService) { }
 
   ngOnInit(): void {
+  }
+
+  toggleExpanded(): void {
+    this.expanded = !this.expanded;
+    
+    if (this.expanded && this.apartamentos.length === 0) {
+      this.carregarApartamentos();
+    }
+  }
+
+  carregarApartamentos(): void {
+    this.carregando = true;
+    this.erro = '';
+    
+    this.listarApartamentosService.listarApartamentosPorTerritorio(this.territorioDTO.territorioID)
+      .subscribe({
+        next: (dados) => {
+          this.apartamentos = dados;
+          this.carregando = false;
+        },
+        error: (erro) => {
+          console.error('Erro ao carregar apartamentos:', erro);
+          this.erro = 'Erro ao carregar apartamentos';
+          this.carregando = false;
+        }
+      });
   }
 
 }
